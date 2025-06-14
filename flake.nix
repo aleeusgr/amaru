708e6265dbc7536e7bfacadbf1ff939c5635af7d
Alex <alexeusgr@gmail.com>
2025-06-14 08:14:47 +0700

    fix tmux error

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
           shellHook = ''
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+            export SHELL=/run/current-system/sw/bin/bash
           '';
 
           # Add precompiled library to rustc search path
